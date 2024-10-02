5df609d52e375c0d509e1c18369513d44cf19927
kaabiiii <kaabimrfuad1@gmail.com>
2024-10-02 10:32:27 +0700

    Add files via upload
    bismillah

---
+++ Pemilihan2Percobaan112.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Pemilihan2Percobaan112 {
+
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan tahun");
+        int tahun = sc.nextInt();
+
+        if ((tahun % 4) == 0) {
+            if ((tahun % 100) != 0)
+            System.out.println("Tahun Kabisat");
+            
+        }else    
+            System.out.println("Bukan Tahun Kabisat");
+            
+            
+    }
+}
